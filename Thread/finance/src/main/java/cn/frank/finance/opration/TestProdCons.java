ad3edda0d4db81213a8ec4d9b564c28621c2906f
zhangfan <63943806+FrankZ20@users.noreply.github.com>
2022-07-18 18:28:30 +0800

    之前忘提交的打

---
+++ Thread/finance/src/main/java/cn/frank/finance/opration/TestProdCons.java
@@ -0,0 +1,108 @@
+package cn.frank.finance.opration;
+
+import lombok.Data;
+
+/**
+ * @author zhangfan
+ * @date 2022/7/18 14:17
+ */
+//测试：生产者消费者模型-->利用缓冲区解决：管程法
+public class TestProdCons {
+}
+//生产者
+class Producer extends Thread {
+
+    SynContainer container;
+    public Producer(SynContainer container) {
+        this.container = container;
+    }
+
+    @Override
+    public void run() {
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("生产者生产了：" + i);
+            container.put(new Product(i));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+//缓冲区
+class SynContainer {
+
+    private static final int DEFAULT_SIZE = 10;
+
+    private int count = 0;
+
+    private static final Product[] chickens;
+
+    static {
+
+        chickens = new Product[DEFAULT_SIZE];
+    }
+
+    //生产者放入产品
+    public synchronized void put(Product chicken) {
+
+        if(count == chickens.length) {
+
+            //通知消费者消费，等待生产
+        }
+        //放入产品
+        chickens[count] = chicken;
+        count++;
+        //通知消费者消费
+
+    }
+
+    public synchronized Product pop() {
+
+        //判断能否消费
+        if(count == 0) {
+
+            //通知生产者生产，等待消费
+        }
+
+        //如果可以消费
+        count--;
+        Product product = chickens[count];
+        //吃完了通知生产者生产
+
+        return product;
+    }
+}
+//消费者
+class Consumer extends Thread {
+
+    SynContainer container;
+
+    public Consumer(SynContainer container) {
+        this.container = container;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("消费者消费了：" + container.pop().getId());
+
+        }
+    }
+}
+
+//产品
+@Data
+class Product {
+
+    int id;
+
+    public Product(int id) {
+        this.id = id;
+    }
+}
